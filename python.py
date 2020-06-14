eecc45b8f7a6d79f24f6bb498f5bba0e62d7e7f6
y2kwak <59381141+y2kwak@users.noreply.github.com>
2020-06-14 23:43:46 +0900

    complete task2

--- python.py
+++ python.py
@@ -1,7 +1,26 @@
 def two_ints(N,M):
+	list = []
 	for i in range(N):
 		if i%M == 0:
-			print(i)
+			list.append(i)
+	print(list)
 #calling the function
-two_ints(6, 2)
 two_ints(12,2)
+
+#defining the function
+def one_int(N):
+	dict = {}
+	squares = []
+	powers = []
+	for i in range(N**2+1):
+		if i**(1/2) <= N and (i**(1/2))%1==0:
+			squares.append(i)
+		if 2**i <=N:
+			powers.append(i)
+	dict["squares"] = squares
+	dict["powers"] = powers
+	print(dict)
+
+#calling the function
+one_int(4)
+one_int(3)
