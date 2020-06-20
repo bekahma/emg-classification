5a5e776584c3390b11882a1bd3a711a0c218728a
y2kwak <59381141+y2kwak@users.noreply.github.com>
2020-06-21 00:38:21 +0900

    pandas dumbdumb

--- data.py
+++ data.py
@@ -1,17 +1,10 @@
 import numpy as np
 import pandas as pd
 
-file1 = pd.read_csv("./EMG_data/01/1_raw_data_13-12_22.03.16.txt")
+df = pd.read_csv("./EMG_data/01/1_raw_data_13-12_22.03.16.txt")
 
-# print(file1.describe())
-print(file1.columns)
+print(df)
+print(df.columns)
+print(type(df.columns))
 
-df = file1.astype({"class": int})
-
-
-# .astype('int')
-
-# df.astype('str')
-# file1.close()
-
-# print file1.describe()
+df["columns"] = df["columns"].to_numeric(int)
