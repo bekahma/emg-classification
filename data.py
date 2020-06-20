4862af5680adb4093a0321d53210f89bdef15e02
y2kwak <59381141+y2kwak@users.noreply.github.com>
2020-06-21 00:08:12 +0900

    reading1

--- data.py
+++ data.py
@@ -1,3 +1,10 @@
 import numpy as np
 import pandas as pd
 
+file1 = pd.read_csv("./EMG_data/01/1_raw_data_13-12_22.03.16.txt")
+
+print(file1.describe())
+
+# file1.close()
+
+# print file1.describe()
