a2ac5afa4eb1c3beedaf58509160bf6d66fe2de0
y2kwak <59381141+y2kwak@users.noreply.github.com>
2020-06-21 00:57:28 +0900

    task3 q4

--- data.py
+++ data.py
@@ -1,10 +1,11 @@
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv("./EMG_data/01/1_raw_data_13-12_22.03.16.txt")
+df = pd.read_csv('./EMG_data/01/1_raw_data_13-12_22.03.16.txt', sep="\t")
 
 print(df)
 print(df.columns)
-print(type(df.columns))
 
-df["columns"] = df["columns"].to_numeric(int)
+df['class'] = df['class'].astype(int)
+
+print(df.dtypes)
