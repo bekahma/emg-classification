0013d3efd74c60be0ae1efe6e7e3325054e35d9f
rebeccama <r69ma@users.noreply.github.com>
2020-07-04 10:12:29 -0400

    plot 3

--- data.py
+++ data.py
@@ -23,16 +23,21 @@ channel3_col = df.loc[:,'channel3']
 t = time_col.values
 cl = class_col.values
 ch = channel1_col.values
+ch2 = channel2_col.values
+ch3 = channel3_col.values
 
 print("\nnumpy arrays for time, class, channel1: ")
 print(t)
 print(cl)
 print(ch)
 
-# multiply channel1 data by 1000 (convert to mV)
+# multiply channel data by 1000 (convert to mV)
 ch_mv = ch * 1000
 print("\nchannel1 data in mV: ")
 print(ch_mv)
+
+ch_mv2 = ch2 * 1000
+ch_mv3 = ch3 * 1000
 
 # divide time data by 1000 (convert to sec)
 t_sec = t / 1000
@@ -40,29 +45,13 @@ print("\ntime data in sec: ")
 print(t_sec)
 
 # plot1: ch_mv vs. t_sec
-# plt.plot(t_sec, ch_mv)
-# plt.ylabel('channel1 (mV)')
-# plt.xlabel('time in (s)')
-# plt.title('plot 1: EMG signal vs. time')
+plt.plot(t_sec, ch_mv)
+plt.ylabel('channel1 (mV)')
+plt.xlabel('time in (s)')
+plt.title('plot 1: EMG signal vs. time')
 # plt.show()
 
 # plot 2: ch_mv vs. t_sec (red when class = 2)
-# make an array of strings w the same length as signal (black, black, red..etc)
-colour_signal = []
-# colour_signal array must be the same length as class array
-for i in cl:
-    # hand clenched = red colour in plot
-    # ^ how do we know h
-    if i == 2:
-        colour_signal.append('r')
-    else:
-        colour_signal.append('b')
-print("\ncolour_signal:")
-print(colour_signal)
-
-# plt.plot(t_sec, ch_mv, color='colour_signal')
-# plt.show()
-
 # <----------------- yewon's attempt...? ----------------->
 # sections of the signal where class = 2
 first_signal = ch_mv[cl == 2]
@@ -77,17 +66,18 @@ first_time = t[cl != 2]
 plt.plot(first_time, first_signal, color='blue')
 
 # show the plot for the first signal
-plt.show()
-
-# length = len(cl)
-# plt.plot(t_sec, ch_mv, color='blue')
-# for i in range(length):
-#    if cl[i] == 2:
-#       plt.plot(t_sec[i], ch_mv[i], color='red')
-
-# plt.title('plot 2: EMG signal vs. time')
-# plt.ylabel('channel1 (mV)')
-# plt.xlabel('time (s)')
 # plt.show()
+
+# plot 3: channel 1 to 3 plot (same colour)
+plt.figure()
+plt.subplot(311)
+plt.plot(t_sec, ch_mv)
+
+plt.subplot(312)
+plt.plot(t_sec, ch_mv2)
+
+plt.subplot(313)
+plt.plot(t_sec, ch_mv3)
+plt.show()
 
 
